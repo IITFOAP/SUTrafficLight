ddefcbca423dad37ad47f7f8a67d03d292b9a140
Baranov Roman <palamarmle@gmail.com>
2023-06-09 14:01:10 +0300

    Made all the logic and interface

--- SUTrafficLight/ContentView.swift
+++ SUTrafficLight/ContentView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redOpacity = 0.5
+    @State private var yellowOpacity = 0.5
+    @State private var greenOpacity = 0.5
+    @State private var hasStarted = true
+    @State private var colorForComparison = SignalColor.red
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            CircleView(color: .red, opacity: redOpacity)
+                .padding(.bottom, 8)
+            CircleView(color: .yellow, opacity: yellowOpacity)
+                .padding(.bottom, 8)
+            CircleView(color: .green, opacity: greenOpacity)
+            
+            Spacer()
+            
+            Button {
+                hasStarted = false
+                
+                switch colorForComparison {
+                case .red:
+                    greenOpacity = 0.5
+                    redOpacity = 1
+                    colorForComparison = .yellow
+                case .yellow:
+                    redOpacity = 0.5
+                    yellowOpacity = 1
+                    colorForComparison = .green
+                case .green:
+                    yellowOpacity = 0.5
+                    greenOpacity = 1
+                    colorForComparison = .red
+                }
+            } label: {
+                Capsule()
+                    .foregroundColor(.blue)
+                    .frame(width: 150, height: 50)
+                    .overlay(Capsule().stroke(Color.white, lineWidth: 4))
+                    .overlay(
+                        Text(hasStarted ? "START" : "NEXT")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    )
+                    .shadow(radius: 10)
+            }
         }
+        
         .padding()
     }
 }
@@ -22,5 +62,11 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    enum SignalColor {
+        case red, yellow, green
     }
 }
---
+++ SUTrafficLight/СircleView.swift
@@ -0,0 +1,28 @@
+//
+//  СircleView.swift
+//  SUTrafficLight
+//
+//  Created by Рома Баранов on 08.06.2023.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var color: Color
+    var opacity: Double
+    
+    var body: some View {
+        Circle()
+            . foregroundColor(color)
+            .frame(width: 100, height: 100)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+            .opacity(opacity)
+    }
+}
+
+struct CircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleView(color: .red, opacity: 0.5)
+    }
+}
